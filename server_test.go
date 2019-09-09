69b3bcb6454e47c532466adac24b45a111370a5a
Andres Rios <rianby64@gmail.com>
2019-09-10 00:32:31 +0300

    Pruebe la DB

--- server_test.go
+++ server_test.go
@@ -1,7 +1,26 @@
 package dbbus
 
-import "testing"
+import (
+	"database/sql"
+	"testing"
+)
 
-func Test_check(t *testing.T) {
-	t.Log("Success")
+var connStr = "host=arca-dbbus-db user=test dbname=test password=test port=5432 sslmode=disable"
+
+func connect() (db *sql.DB, err error) {
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		return
+	}
+
+	err = db.Ping()
+	return
+}
+
+func Test_check_db(t *testing.T) {
+	_, err := connect()
+
+	if err != nil {
+		t.Error(err)
+	}
 }
