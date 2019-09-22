27f03d28b871aaaad6a79b3c1f0759b0028d4264
Andres Rios <rianby64@gmail.com>
2019-09-22 09:01:27 +0300

    Aisle las pruebas

--- run-db.sh
+++ run-db.sh
@@ -4,4 +4,4 @@
 docker network create arca-dbbus-net
 
 docker build -t arca-dbbus-go -f go.Dockerfile . && \
-docker run -d --rm --name arca-dbbus-db --net arca-dbbus-net -p 5432:5432 arca-dbbus-db
+docker run -d --rm --name arca-dbbus-db --net arca-dbbus-net arca-dbbus-db
