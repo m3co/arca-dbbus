1a9a40788f0b204d551594b82e7edf268920b962
Andres Rios <rianby64@gmail.com>
2020-04-15 02:33:04 +0300

    [CRITICAL] Elimine tipos innecesarios

--- types.go
+++ types.go
@@ -23,54 +23,10 @@ type Notification struct {
 		 * Target es a que tabla/vista está dirigido el JSON-RPC
 		 * Db es el nombre de la base de datos
 		 * Prime es si Target esta dirigido a una Tabla
-		     true  - Target va hacia una tabla "primaria"
-				 false - Target va hacia una vista
+		    true  - Target va hacia una tabla "primaria"
+			false - Target va hacia una vista
 	*/
 	Context map[string]interface{}
-}
-
-// ComboboxInfo contiene la info sobre un combobox
-/* Ejemplo:
-```
-  Source: "FACAD-ParamsBIC",
-  Display: "Field",
-  Value: "Field",
-  Params: {
-    "BuiltInCategories": "BuiltInCategories",
-    "ReportType":  "ReportType",
-    "Field": "ConstraintField",
-  }
-```
-*/
-type ComboboxInfo struct {
-	Source  string
-	Display string
-	Value   string
-	Params  map[string]string
-}
-
-// FieldInfo representa la informacion sobre un campo
-type FieldInfo struct {
-	Name     string
-	Type     string
-	Primary  bool
-	Required bool
-	Editable bool
-	Combobox *ComboboxInfo
-	Select   *[]string
-}
-
-// ActionsInfo representa las acciones disponibles para una tabla o vista
-type ActionsInfo struct {
-	Insert bool
-	Delete bool
-	Update bool
-}
-
-// ModelInfo representa la informacion sobre una tabla o vista
-type ModelInfo struct {
-	Actions ActionsInfo
-	Fields  []FieldInfo
 }
 
 // Server grid that binds all the DBs with the json-rpc arca server
